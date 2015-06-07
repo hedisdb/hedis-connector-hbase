8a7091fa6cb46ff85bb00107935e76f9edc308de
kewang <cpckewang@gmail.com>
2015-06-07 23:59:59 +0800

    test cross project header file

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include "hedis.h"
 
-int init(){
+int init(hedisConfigEntry *entry){
+	printf("%s: %s\n", entry->key, entry->value);
 	printf("hello world connector\n");
 
 	return 0;
