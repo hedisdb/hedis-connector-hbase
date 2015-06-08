39938ce0b14ebd967a143b74dafda7e405bebedf
kewang <cpckewang@gmail.com>
2015-06-08 21:22:46 +0800

    complete entry load

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include "hedis.h"
 
-int init(hedisConfigEntry *entry){
-	printf("%s: %s\n", entry->key, entry->value);
+hedisConfigEntry **hedis_entries;
+
+int init(hedisConfigEntry **entries, int entry_count){
 	printf("hello world connector\n");
 
+	hedis_entries = entries;
+
+	for(int i = 0; i < entry_count; i++){
+		printf("%s: %s\n", entries[i]->key, entries[i]->value);
+	}
+
 	return 0;
+}
+
+char *get_value(){
+	return hedis_entries[0]->key;
 }
