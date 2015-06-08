8b702b3033cb56b18106fa8c5cc91129370c636c
kewang <cpckewang@gmail.com>
2015-06-08 22:24:30 +0800

    test get value from global variable

--- main.c
+++ main.c
@@ -2,11 +2,13 @@
 #include "hedis.h"
 
 hedisConfigEntry **hedis_entries;
+int hedis_entry_count;
 
 int init(hedisConfigEntry **entries, int entry_count){
-	printf("hello world connector\n");
+	printf("hello world init\n");
 
 	hedis_entries = entries;
+	hedis_entry_count = entry_count;
 
 	for(int i = 0; i < entry_count; i++){
 		printf("%s: %s\n", entries[i]->key, entries[i]->value);
@@ -16,5 +18,7 @@ int init(hedisConfigEntry **entries, int entry_count){
 }
 
 char *get_value(){
+	printf("hello world get_value\n");
+
 	return hedis_entries[0]->key;
 }
