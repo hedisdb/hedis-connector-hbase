312b204f6d0c45d27758807c015c60d42a809e25
kewang <cpckewang@gmail.com>
2015-09-26 11:23:45 +0800

    Remove subtree command, because it should run as developer

--- libhbase.sh
+++ libhbase.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-git subtree add --prefix third_party/libhbase git@github.com:mapr/libhbase master --squash
-git subtree pull --prefix third_party/libhbase git@github.com:mapr/libhbase master --squash
-
 cd third_party/libhbase
 
 mvn install -DskipTests
